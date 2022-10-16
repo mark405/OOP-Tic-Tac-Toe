22816f6b5a2c5ec3a89df26270fa7c4504fe977b
mark405 <mark200320031.tun@gmail.com>
2022-10-17 02:55:55 +0300

    Override toString method in model classes

--- src/mark/tictactoe/model/GameTable.java
+++ src/mark/tictactoe/model/GameTable.java
@@ -17,6 +17,8 @@
 
 package mark.tictactoe.model;
 
+import java.util.Arrays;
+
 /**
  * @author mark
  */
@@ -38,5 +40,23 @@ public class GameTable {
 
     public void srtSign(final Cell cell, final char sign) {
         table[cell.getRow()][cell.getCol()] = sign;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("GameTable{");
+
+        sb.append("table=");
+
+        for (int i = 0; i < table.length; i++) {
+            sb.append(Arrays.toString(table[i]));
+            if(i < table.length - 1) {
+                sb.append(';');
+            }
+        }
+        
+        sb.append('}');
+
+        return sb.toString();
     }
 }
