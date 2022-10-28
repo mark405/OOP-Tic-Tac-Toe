cafe56e3a9f634086757cc4cd042dd848878f8c2
mark405 <mark200320031.tun@gmail.com>
2022-10-28 03:02:39 +0300

    Set 2.0 version

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-# Copyright (c) 2019. Mark Zavgorodnii
+# Copyright (c) 2019. http://devonline.academy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,11 +13,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#
-
-cd "$(dirname "$0")" || exit
-
-java -jar ${project.build.finalName}.jar
+########################################################################################################################
+# The sh script to start the tic-tac-toe game for Unix systems                                                         #
+#                                                                                                                      #
+# @author devonline                                                                                                    #
+# @email  devonline.academy@gmail.com                                                                                  #
+########################################################################################################################
+# ----------------------------------------------------------------------------------------------------------------------
+# Try to use the `java` from `$JAVA_HOME` if this environment variable set correctly:
+if [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/java" ] ; then
+  JAVA_CMD="$JAVA_HOME/bin/java"
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+# Try to use the `java` using `PATH` environment variable:
+WHICH_JAVA=$(which java)
+if [ -x "$WHICH_JAVA" ]; then
+  JAVA_CMD="java"
+  unset WHICH_JAVA
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+# Try to use `java` from JRE if `jre/bin/java` exists and executable:
+cd "$(dirname"$0")" || exit
+if [ -x "jre/bin/java" ]; then
+  JAVA_CMD="jre/bin/java"
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+if [ -z ${JAVA_CMD+x} ]; then
+  # Throw error if `java` is not configured:
+  echo "------------------------------------------------------------------------" >&2
+  echo "\`java\` not defined! Install or configure JVM before using this script!" >&2
+  echo "------------------------------------------------------------------------" >&2
+  RETURN_CODE=1
+else
+  # Fix current dir issue. Read more: http://hints.macworld.com/article.php?story=20041217111834902
+  cd "$(dirname "$0")" || exit
+  # Run tic-tac-toe game:
+  $JAVA_CMD -jar ${project.build.finalName}.jar
+  RETURN_CODE=0
+fi
+# ----------------------------------------------------------------------------------------------------------------------
+# Wait for the `enter` key pressed:
 echo "Press enter to continue . . ."
 read -r test
+# ----------------------------------------------------------------------------------------------------------------------
+exit $RETURN_CODE
